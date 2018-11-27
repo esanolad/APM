99ca7ec2d6b4c2ab4e77cc5fa6b5799b8f22a0af
esanolad <esanolad@gmail.com>
2018-11-27 15:11:30 +0100

    route advance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { RouterModule} from "@angular/router";
     HttpClientModule,
     RouterModule.forRoot([
       {path:'products',component:ProductListComponent},
-      {path:'products:/id',component:ProductDetailComponent},
+      {path:'products/:id',component:ProductDetailComponent},
       {path:'welcome',component:WelcomeComponent},
       {path:'',redirectTo:'welcome',pathMatch:'full'},
       {path:'**',redirectTo:'welcome', pathMatch:'full'}])
--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from "./product";
+import {ActivatedRoute, Router} from '@angular/router'
+import { ProductService } from './product.service';
 
 
 @Component({
@@ -8,12 +10,24 @@ import { IProduct } from "./product";
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
   product:IProduct;
   pageTitle:string='Product Detail';
-  constructor() { }
+  constructor(private route:ActivatedRoute, private productService:ProductService, private router:Router) {
+    
+   }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getProducts().subscribe((prod)=>{
+      //console.log(prod[1]);
+      //this.product=prod;
+      prod.forEach((p)=>{
+        if (p.productId==id) this.product=p;
+      })
+    });
+  }
+  onBack():void{
+    this.router.navigate(['/products']);
   }
 
 }
